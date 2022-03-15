04515a9a06b4f7c869e4e79efab0ec6b521b318a
Luiz Vieira <luiz22_sp@hotmail.com>
2022-03-15 20:24:20 -0300

    04 API externa e interface

--- app/dist/js/controllers/negociacao-controller.js
+++ app/dist/js/controllers/negociacao-controller.js
@@ -10,6 +10,7 @@ import { logarTempoDeExcucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
+import { NegociacoesService } from '../services/negociacoes-service.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 export class NegociacaoController {
@@ -17,18 +18,27 @@ export class NegociacaoController {
         this.negociacoes = new Negociacoes();
         this.negociacoesView = new NegociacoesView('#negociacoesView');
         this.mensagemView = new MensagemView('#mensagemView');
+        this.negociacoesService = new NegociacoesService();
         this.negociacoesView.update(this.negociacoes);
     }
     adiciona() {
         const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         if (!this.ehDiaUtil(negociacao.data)) {
-            this.mensagemView
-                .update('Apenas negociações em dias úteis são aceitas');
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
             return;
         }
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+    }
+    importaDados() {
+        this.negociacoesService.obterNegociacoesDoDia()
+            .then(negociacoesDeHoje => {
+            for (let negociacao of negociacoesDeHoje) {
+                this.negociacoes.adiciona(negociacao);
+            }
+            this.negociacoesView.update(this.negociacoes);
+        });
     }
     ehDiaUtil(data) {
         return data.getDay() > DiasDaSemana.DOMINGO
--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -2,8 +2,10 @@ import { domInjector } from '../decorators/dom-injector.js';
 import { Inpect } from '../decorators/inspect.js';
 import { logarTempoDeExcucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { NegociacoesDoDia } from '../interfaces/negociacoes-do-dia.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
+import { NegociacoesService } from '../services/negociacoes-service.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
@@ -14,10 +16,11 @@ export class NegociacaoController {
   private inputQuantidade: HTMLInputElement;
   @domInjector('#valor')
   private inputValor: HTMLInputElement;
-
+  
   private negociacoes = new Negociacoes();
   private negociacoesView = new NegociacoesView('#negociacoesView');
   private mensagemView = new MensagemView('#mensagemView');
+  private negociacoesService = new NegociacoesService();
 
   constructor() {
     // this.inputData = <HTMLInputElement>document.querySelector('#data');
@@ -39,14 +42,23 @@ export class NegociacaoController {
     );
 
     if (!this.ehDiaUtil(negociacao.data)) {
-      this.mensagemView
-        .update('Apenas negociações em dias úteis são aceitas');
+      this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
       return;
     }
 
     this.negociacoes.adiciona(negociacao);
     this.limparFormulario();
     this.atualizaView();
+  }
+
+  importaDados(){
+    this.negociacoesService.obterNegociacoesDoDia()
+      .then(negociacoesDeHoje => {
+        for(let negociacao of negociacoesDeHoje){
+          this.negociacoes.adiciona(negociacao);
+        }
+        this.negociacoesView.update(this.negociacoes);
+      });
   }
 
   private ehDiaUtil(data: Date) {
@@ -65,4 +77,5 @@ export class NegociacaoController {
     this.negociacoesView.update(this.negociacoes);
     this.mensagemView.update('Negociação adicionada com sucesso');
   }
+
 }
